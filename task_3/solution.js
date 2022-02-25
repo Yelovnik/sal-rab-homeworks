3303e845024808cf3dee3ceb116189ae4a7f4884
Yelovnik <nromasenko@yandex.ru>
2022-02-25 11:57:04 +0300

    test_3 done

--- task_3/solution.js
+++ task_3/solution.js
@@ -2,7 +2,7 @@ function renderCartItem(item) {
     const product = item;
 
     // Задание №3.1. Формирование строки корзины
-
+        
     // product - объект вида {id: id, title: title, count: count, price: price}
     // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
     // id - идентификатор
@@ -10,6 +10,15 @@ function renderCartItem(item) {
     // price - цена
     // count - количество
 
+let itemCountText = '';
+itemCountText = itemCountText + product.count;
+itemCountText = itemCountText + ` × `;
+itemCountText = itemCountText + product.price;
+itemCountText = itemCountText + ' ₽ = ';
+let sum = product.count * product.price;
+itemCountText = itemCountText + sum;
+itemCountText = itemCountText + ' ₽';
+    
     // сформируйте строку itemCountText вида:
     // количество × цена ₽ = стоимость ₽
     // например,
